ff94d3ecebaab698b60e1058e5b6f75610dfcc01
NathanRicardo34 <nathanricardo34@gmail.com>
2023-06-06 11:06:37 -0400

    add user CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TasksModule, MongooseModule.forRoot('mongodb+srv://db_user:WwbcNcLVkRaTatjQ@exemplos.xmdrwte.mongodb.net/?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
