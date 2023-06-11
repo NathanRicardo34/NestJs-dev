f699fbdcaacd4053f5e969aac56e1cab6110d02a
NathanRicardo34 <nathanricardo34@gmail.com>
2023-06-11 15:37:42 -0400

    aumentando o tempo para expirar token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { jwtConstants } from './shared/jwt-auth/constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' }
+      signOptions: { expiresIn: '360s' }
     })
   ],
   controllers: [AuthController],
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 5555
+  app.enableCors({
+    origin: '*'
+  })
+  await app.listen(port);
+  console.log(`localhost:${port}`)
 }
 bootstrap();
